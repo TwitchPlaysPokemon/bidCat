2462c1249bb0dc28b0ebb115f7d93a15b0755b32
hlixed <onehlixed1@yahoo.com>
2015-12-02 17:43:26 -0500

    Initial commit of auctionsys

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # bidcat 
 
-A system to handle song bidding in PBR; or really any case case where there's a single choice among many and tokens are used to vote on it.
-
-Used in musicCat.
+A system to handle bidding, supporting collaborative bidding and other features
---
+++ auctionsys.py
@@ -0,0 +1,96 @@
+"""Logic to handle multiple users making "up-to" bids collaboratively.
+
+Each user places bids on items by telling the auction system the most they're willing to bid and
+have the system automatically bid on their behalf with the intent of securing the lowest price
+possible, for example, if Alice bids 100 on "Item A" and Bob bids 1 on "Item B" Alice should win and pay 2.
+
+Multiple users able to bid on the same item with their combined bids beating an overall smaller single bid.
+
+All references to "money" in this module refers to an arbitrary indivisible integer unit of currency.
+
+What is bidded on are called "items" and are referenced by strings or integers.
+
+User IDs are strings or integers.
+"""
+
+import logging
+
+from banksys import InsufficientMoneyError
+
+
+class Auction(object):
+    """Handles multiple users bidding on multiple items, only one item can win.
+
+    All provided item IDs and user IDs are assumed to be valid.
+    """
+    def __init__(self, bank):
+        """
+        Arguments:
+            bank: bank object to access to reserve currency
+        """
+        # track bank
+        self.bank = bank
+        # set up logging
+        self.log = logging.getLogger("auctionsys")
+
+    def register_reserved_money_checker(self):
+        """Adds the reserved money checker function at the bank.
+
+        If this is used the function MUST be removed before the auction object is deleted!
+        """
+        self.log.info("registering reserved money checker")
+        self.bank.reserved_money_checker_functions.add(self.get_reserved_money)
+
+    def deregister_reserved_money_checker(self):
+        """Removes the reserved money checker function from the bank.
+
+        This MUST be called when the auction has been finished and fulfilled.
+        """
+        self.log.info("deregistering reserved money checker")
+        self.bank.reserved_money_checker_functions.remove(self.get_reserved_money)
+
+    def get_reserved_money(self, user_id):
+        """Calculate the amount of money a user has tied up in the auction system.
+
+        It is guaranteed that no more than this amount will be taken from the
+        user's account without furthur action from this user.
+        """
+        return 0
+
+    def place_bid(self, user_id, item_id, max_bid):
+        if max_bid <= 0:
+            raise ValueError("'max_bid' must be a value above 0")
+        # ensure the user can afford it
+        available_money = self.bank.get_available_money(user_id)
+        reserved_money = self.bank.get_reserved_money(user_id)
+        # TODO: consider how much is reserved by previous bids existence' on this item
+        # determine amount of money spent on this
+        if max_bid > available_money + reserved_money:
+            raise InsufficientMoneyError("can't afford to make bid")
+
+    def process_bids(self):
+        """Process everyone's bids and make any changes.
+
+        Returns:
+            dict containing information about what happened and the new state of the auction.
+        """
+        return {"hello": "world"}
+
+
+def main():
+    from banksys import DummyBank
+    logging.basicConfig(level=logging.DEBUG)
+    bank = DummyBank()
+    auction = Auction(bank=bank)
+    auction.register_reserved_money_checker()
+    auction.place_bid("bob", "pepsiman", 1)
+    print(auction.process_bids())
+    auction.place_bid("alice", "katamari", 2)
+    print(auction.process_bids())
+    auction.place_bid("bob", "pepsiman", 2)
+    print(auction.process_bids())
+    auction.deregister_reserved_money_checker()
+
+
+if __name__ == "__main__":
+    main()
