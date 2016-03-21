39a771666ea679516685018b1f28e4802bc525c3
Felk <studio.felk@googlemail.com>
2016-03-21 16:39:17 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+setup(
+    name="bidcat",
+    version="1.0",
+    packages=["bidcat"],
+
+    author="1hlixed",
+    description="TPP BidCat collaborative auction-style bidding library",
+    url="https://github.com/TwitchPlaysPokemon/bidCat",
+)
